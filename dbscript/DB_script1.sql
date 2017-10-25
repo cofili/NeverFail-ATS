8856e5039e3969cd4b652c0cfbedcd65bf59bc44
Aziz <aalibra@stedwards.edu>
2017-10-25 17:52:51 -0500

    essagegetTest() API

--- dbscript/DB_script1.sql
+++ dbscript/DB_script1.sql
@@ -1,9 +1,10 @@
 create table tests
  (testId int auto_increment not null,
-  testName varchar(50),
+  testDesc varchar(50),
   scriptName varchar(50),
-  primary key (testId),
+  testDailyFreqCount int,
+  primary key (testId)
  )Engine=InnoDB;
  
  
- insert into classroom tests (Null, 'directory list', 'directoryList.py');
+
