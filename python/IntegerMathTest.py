fe8a06f4769e25778c67dcc7a36f4e07a6ce1418
Anthony Ofili <ofili.anthony@yahoo.com>
2017-11-19 16:25:19 -0600

    Integer Math Test

---
+++ python/IntegerMathTest.py
@@ -0,0 +1,42 @@
+#TBD
+#Integer Math test: Measures how fast the CPU can perform integer operations. 
+ 
+import os
+import time
+import math 
+
+from arithmetic import Rational
+
+rational = Rational()
+
+ class Rational:
+    """ A python class for basic arithmetic operations. With 2 rational
+    numbers class can be used to add, subtract, divide and multiply.
+    """
+
+    def add(self, x, y):
+    #  Calculates addition of x and y and return the result. 
+         return x + y
+
+    def subtract(self, x, y):
+    # Calculates subtraction of x and y and return the result.
+        return x - y
+
+    def divide(self, x, y):
+    #  Calculates division of x and y and return the result.
+        return x / y
+
+    def multiply(self, x, y):
+    #  Calculates multiplication from  x and y and return the result.
+        return x * y
+
+ def main():
+ 
+
+    start_time = time.time()
+  
+
+
+
+
+    main()
